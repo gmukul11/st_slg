67ac13600d7e42ca91e263c3bbc5de7b44764f52
Mukul <54817117+gmukul11@users.noreply.github.com>
2023-04-05 18:01:43 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -13,7 +13,7 @@ def generate_response(prompt):
 def ctr_prediction(inpt,channel):
     prompt="Channel: " + channel + ", Title: "+ inpt +  " ->"
     prompt=prompt.strip()
-    res=openai.Completion.create(model="davinci:ft-netcore-cloud:travel-industry-ctr-pred-model-2023-04-04-19-33-44", prompt=prompt,max_tokens=5)['choices'][0]['text'].strip()
+    res=openai.Completion.create(model="davinci:ft-netcore-cloud:travel-industry-ctr-pred-model-2023-04-04-19-33-44", prompt=prompt,max_tokens=5,temperature=0)['choices'][0]['text'].strip()
     try:
         res=float(res)
     except:
