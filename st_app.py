7dc64638d8c80524a4ca0fd63c28c409b3f3041e
Mukul <54817117+gmukul11@users.noreply.github.com>
2023-04-05 14:08:57 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -64,7 +64,7 @@ if __name__=="__main__":
         try:
             
             if slg_scr_li[i]>input_scr:
-                st.markdown(f"Suggestion: {slg_li[i][2:].strip().split("(")[0}") 
+                st.markdown(f"Suggestion: {slg_li[i][2:].strip().split('(')[0}") 
                 try:
                     st.markdown(f"Expected click rate: {slg_scr_li[i]:.02f}%")
                 except:
