c0316a7a85e972e8ee5b6a6c4283eee8dc66bd7f
Mukul <54817117+gmukul11@users.noreply.github.com>
2023-04-06 09:36:58 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -62,11 +62,12 @@ if __name__=="__main__":
     st.subheader("Suggested Market campaign title based on historical insight and client input :")
     for i in range(len(slg_li)):
         try:
-            st.markdown(f"Suggestion: {slg_li[i][2:].strip().split('(')[0]}") 
-            try:
-                st.markdown(f"Expected click rate: {slg_scr_li[i]+0.2:.02f}%")
-            except:
-                st.markdown("not valid score")
+            if slg_scr_li[i]+0.2>input_scr:
+                st.markdown(f"Suggestion: {slg_li[i][2:].strip().split('(')[0]}") 
+                try:
+                    st.markdown(f"Expected click rate: {slg_scr_li[i]+0.2:.02f}%")
+                except:
+                    st.markdown("not valid score")
         except:
             st.markdown("not valid score")
     st.subheader("Historical market campaign title insights :")
