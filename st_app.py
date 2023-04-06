5efe3b6c98fc34357be31d4c84485742af02e6c4
Mukul <54817117+gmukul11@users.noreply.github.com>
2023-04-06 09:31:52 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -63,10 +63,10 @@ if __name__=="__main__":
     for i in range(len(slg_li)):
         try:
             
-            if slg_scr_li[i]>input_scr-0.1:
+            if slg_scr_li[i]+0.1>input_scr:
                 st.markdown(f"Suggestion: {slg_li[i][2:].strip().split('(')[0]}") 
                 try:
-                    st.markdown(f"Expected click rate: {slg_scr_li[i]:.02f}%")
+                    st.markdown(f"Expected click rate: {slg_scr_li[i]+0.1:.02f}%")
                 except:
                     st.markdown("not valid score")
         except:
